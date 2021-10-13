ff8693f7a2d3528bb21d1f545bc213aa8d92490e
ElizabethYasmin <62725994+ElizabethYasmin@users.noreply.github.com>
2021-10-13 10:01:50 -0500

    ++

---
+++ progr.py
@@ -0,0 +1,547 @@
+from time import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = []
+
+with open("25000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+
+data.append(lines)
+file_in.close()
+
+with open("50000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("75000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("100000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+print(len(data))
+with open("125000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("150000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("175000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("200000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("225000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("250000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("275000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("300000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("325000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("350000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("375000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("400000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("425000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("450000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("475000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("500000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("525000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("550000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("575000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("600000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("625000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("650000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("675000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("700000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("725000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("750000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("775000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("800000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("825000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("850000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("875000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("900000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("925000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("950000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("975000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+
+with open("1000000.txt") as file_in:
+	lines = []
+	for line in file_in:
+		lines.append(int(line))
+print(len(data))
+data.append(lines)
+file_in.close()
+print(len(data))
+
+def insertion_sort(arr):
+	start_time = time()
+	
+
+	x = len(arr)
+
+	
+
+	for i in range(1, x):
+		key = arr[i]
+		j = i-1
+		while(j>= 0 and arr[j] > key):
+			arr[j+1] = arr[j]
+			j-=1
+		arr[j+1] = key
+
+
+
+	elapsed_time = time() - start_time
+	#print("Elapsed time: %.10f seconds." % elapsed_time)
+	return elapsed_time
+
+def merge(arr):
+	if len(arr) > 1:
+
+		mid = len(arr)//2
+
+		
+
+		L = arr[:mid]
+		R = arr[mid:]
+
+		merge(L)
+		merge(R)
+
+
+		i = k = j = 0
+
+# Copy data to temp arrays L[] and R[]
+		while i < len(L) and j < len(R):
+
+			if L[i] < R[j]:
+				arr[k] = L[i]
+				i += 1
+			else:
+				arr[k] = R[j]
+				j += 1
+			k += 1
+ 
+ # Checking if any element was left
+		while i < len(L):
+			arr[k] = L[i]
+			i += 1
+			k += 1
+		while j < len(R):
+			arr[k] = R[j]
+			j += 1
+			k += 1
+		return(arr)
+
+def merge_sort(arr):
+
+	start_time = time()
+
+
+	merge(arr)
+
+	elapsed_time = time() - start_time
+
+
+	#print("Elapsed time: %.10f seconds." % elapsed_time)
+	return elapsed_time
+
+def bubbleSort(arr):
+	start_time = time()
+
+	n = len(arr)
+
+	for i in range(n-1):
+		for j in range(0, n-i-1):
+			if arr[j] > arr[j + 1] :
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+	elapsed_time = time()-start_time
+	return elapsed_time
+
+def heapify(arr, n, i):
+	largest = i
+	l = 2 * i + 1  
+	r = 2 * i + 2   
+
+	if l < n and arr[i] < arr[l]:
+		largest = l
+
+	if r < n and arr[largest] < arr[r]:
+		largest = r
+	if largest != i:
+		arr[i],arr[largest] = arr[largest],arr[i] 
+		heapify(arr, n, largest)
+
+def heapSort(arr):
+	start_time = time()
+
+	n = len(arr)
+	for i in range(n // 2 - 1, -1, -1):
+		heapify(arr, n, i)
+
+	for i in range(n-1, 0, -1):
+		arr[i], arr[0] = arr[0], arr[i]  
+		heapify(arr, i, 0)
+
+	elapsed_time = time()-start_time
+	return elapsed_time
+
+def selection_sort(arr):
+
+	start_time = time()
+
+	for i in range(len(arr)):
+		min_idx = i
+		for j in range(i+1, len(A)):
+			if A[min_idx] > A[j]:
+				min_idx = j
+		A[i], A[min_idx] = A[min_idx], A[i]
+
+	elapsed_time = time() - start_time
+	return elapsed_time
+
+def shellSort(arr):
+
+	start_time = time()
+
+	n = len(arr)
+	gap = n/2
+	while gap > 0:
+		for i in range(gap,n):
+
+			temp = arr[i]
+			j = i
+			while  j >= gap and arr[j-gap] >temp:
+				arr[j] = arr[j-gap]
+				j -= gap
+			arr[j] = temp
+		gap /= 2
+
+	elapsed_time = time() - start_time
+	return elapsed_time
+
+def partition(arr, low, high):
+	i = (low-1)
+	pivot = arr[high]     
+
+	for j in range(low, high):
+		if arr[j] <= pivot:
+			i = i+1
+			arr[i], arr[j] = arr[j], arr[i]
+
+	arr[i+1], arr[high] = arr[high], arr[i+1]
+	return (i+1)
+
+def quickSort(arr, low, high):
+	if len(arr) == 1:
+		return arr
+	if low < high:
+		pi = partition(arr, low, high)
+		quickSort(arr, low, pi-1)
+		quickSort(arr, pi+1, high)
+
+def quikS(arr):
+
+	start_time = time()
+
+	quickSort(arr,0,len(arr)-1)
+
+	elapsed_time = time()- start_time
+	return elapsed_time
+
+def copy(arr):
+	A = []
+	for i in range(len(arr)):
+		A.append(arr[i])
+	return A
+
+e = 25000
+
+print("datos\tbubblesort\theapsort\tinsertion_sort\tselection_sort\tshellsort\tmerge_sort\tquicksort")
+
+for i in range(len(data)):
+	print(e, end = "\t")
+
+	e += 25000
+
+	A = copy(data[i])
+
+	print(bubbleSort(data[i]), end = "\t")
+
+	A = copy(data[i])
+
+	print(heapSort(data[i]), end = "\t")
+
+	A = copy(data[i])
+
+	print(insertion_sort(data[i]), end = "\t")
+
+	A = copy(data[i])
+
+	print(selection_sort(data[i]), end = "\t")
+
+	A = copy(data[i])
+
+	print(shellSort(data[i]), end = "\t")
+
+	A = copy(data[i])
+
+	print(merge_sort(data[i]), end = "\t")
+
+	A = copy(data[i])
+
+	print(quickSort(data[i]), end = "\t")
